64410d929b3a23acdd3d88ff313466d5fc7aebef
Ernesto N. Aides <eaides@hotmail.com>
2018-02-12 09:00:07 +0200

    by data start groups of groups, need to fix errors on html validators

--- src/app/components/pordata/pordata.component.ts
+++ src/app/components/pordata/pordata.component.ts
@@ -8,26 +8,45 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class PordataComponent implements OnInit {
 
+  // usuario: Object = {
+  //   nombre: 'Ernesto',
+  //   apellido: 'Aides',
+  //   correo: 'eaides@hotmail.com'
+  // };
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'Ernesto',
+      apellido: 'Aides',
+    },
+    correo: 'eaides@hotmail.com'
+  };
+
   forma: FormGroup;
 
   constructor() {
     this.forma = new FormGroup({
-      nombre: new FormControl('',
-        [
-          Validators.required,
-          Validators.minLength(4)
-        ]),
-      apellido: new FormControl('',
-        [
-          Validators.required,
-          Validators.minLength(5)
-      ]),
+      nombrecompleto: new FormGroup({
+        nombre: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(4)
+          ]),
+        apellido: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(5)
+          ]),
+      }),
       correo: new FormControl('',
         [
           Validators.required,
           Validators.pattern('^[a-z0-9._%+]+@[a-z0-9.-]+\\.[a-z]{2,3}$')
       ])
     });
+
+    // the set value must receive an object with the same 'structure'
+    // of the formgroups anidation
+    this.forma.setValue( this.usuario );
   }
 
   ngOnInit(
@@ -36,5 +55,11 @@ export class PordataComponent implements OnInit {
   guardarCambios() {
     console.log('value', this.forma.value);
     console.log('obj', this.forma);
+
+    this.forma.reset( {
+      nombre: '',
+      apellido: '',
+      correo: ''
+    });
   }
 }
